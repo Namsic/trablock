6d835ddbddae79e4bd8da2663071d50183c929c2
Namsic <kimnj3050@gmail.com>
2020-12-26 00:22:03 +0900

    Update main.dart
    Use array in listview
    Show black page
    Popup dialog

--- trablock_app/lib/main.dart
+++ trablock_app/lib/main.dart
@@ -1,34 +1,31 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+final List<Travel> myTravelList = []; // 로컬 데이타베이스에서 불러올 예정
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'trablock'),
+      initialRoute: MyHomePage.routeName,
+      routes: {
+        MyHomePage.routeName: (context) => MyHomePage(),
+        ShowPlanRoute.routeName: (context) => ShowPlanRoute(),
+      },
     );
   }
 }
 
+// MyHomePage 대신 다른 이름 필요성 느낌
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-
+  static final routeName = '/';
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -67,29 +64,106 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('widget.title'),
       ),
-      body: ListView(  //리스트타일을 고정해놓는것이 아니라 뭔가 새로 만든 객채의 배열을 통해 만들어야 할듯(뇌피셜)
-        children: <Widget>[
-          ListTile(
-            title: Text('여행1'),
-              //onTap: , 탭 되었을때 넘어가는 기능
-          ),
-          ListTile(
-            title: Text('여행2'),
-            //onTap: ,
-          ),
-          ListTile(
-            title: Text('여행3'),
-            //onTap: ,
-          ),
-        ]
-      ),
+      body: TravelListView(),
       floatingActionButton: FloatingActionButton(
-        //onPressed: , 팝업을 통해 새로운 여행 만들 수 있는 기능
+        onPressed: (){
+          showDialog(
+            context: context,
+            barrierDismissible: false,
+            builder: (BuildContext context){
+              return AlertDialog(
+                title: Text('새 여행 만들기'),
+                content: Text('입력칸 추가예정'),
+                actions: <Widget>[
+                  FlatButton(
+                    child: Text("확인"),
+                    onPressed: (){
+                      setState(() {
+                        myTravelList.add(Travel("my_trablock"));
+                      });
+                      Navigator.pop(context);
+                    },
+                  ),
+                  FlatButton(
+                    child: Text(
+                      "취소",
+                      style: TextStyle(color: Colors.red),
+                    ),
+                    onPressed: () => Navigator.pop(context),
+                  ),
+                ],
+              );
+            }
+          );
+        },
         tooltip: '새 여행 시작',
         child: Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+
+class ShowPlanRoute extends StatefulWidget {
+  static final routeName = '/show';
+  @override
+  _ShowPlanRouteState createState() => _ShowPlanRouteState();
+}
+
+class _ShowPlanRouteState extends State<ShowPlanRoute> {
+  // 메인화면에서 각 계획 클릭시 나타날 화면. 작성된 계획을 보기 편하게 보여줘야 할듯
+  @override
+  Widget build(BuildContext context) {
+    final Travel _travel = ModalRoute.of(context).settings.arguments;
+
+    return Scaffold(
+      appBar: AppBar(  // 계획 수정버튼 등 필요
+        title: Text(_travel.title),
+      ),
+    );
+  }
+}
+
+
+class TravelTile extends StatelessWidget{
+  // 리스트뷰에 보여질 각각의 타일들
+  TravelTile(this._travel);
+
+  final Travel _travel;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(_travel.title),
+      onTap: (){  // 클릭 시 해당하는 여행계획 화면으로 이동
+        Navigator.pushNamed(
+            context,
+            ShowPlanRoute.routeName,
+            arguments: _travel
+        );
+      },
+    );
+  }
+}
+
+class TravelListView extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: myTravelList.length,
+      itemBuilder: (BuildContext context, int index){
+        return TravelTile(myTravelList[index]);
+      },
+    );
+  }
+}
+
+
+class Travel {
+  // 각각의 여행. 여러 개의 계획으로 구성되어 있(을 예정이)다.
+  String title;
+
+  Travel(this.title);
 }
